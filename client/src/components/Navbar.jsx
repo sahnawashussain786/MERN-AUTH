53fbd1bdb740c6ec2c742fd71c00b4d696b3a73b
Sahnawas Hussain <142085109+sahnawashussain786@users.noreply.github.com>
2024-12-08 14:31:40 +0530

    connection login and logout func

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,21 +1,37 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const { userData, backendUrl, setUserData, setIsLoggedin } = useContext(
+    AppContext
+  );
 
   return (
     <div className="w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0">
       <img src={assets.logo} className="w-28 sm:w-32" alt="" />
-      <button
-        onClick={() => navigate("/login")}
-        className="flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all duration-300"
-      >
-        Login
-        <img src={assets.arrow_icon} alt="" />
-      </button>
+      {userData ? (
+        <div className="w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group">
+          {userData.name[0].toUpperCase()}
+          <div className="absolute hidden group-hover:block top-0 ring-0 z-10 text-black rounded pt-10">
+            <ul className="">
+              <li>Verify Email</li>
+              <li>Logout</li>
+            </ul>
+          </div>
+        </div>
+      ) : (
+        <button
+          onClick={() => navigate("/login")}
+          className="flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all duration-300"
+        >
+          Login
+          <img src={assets.arrow_icon} alt="" />
+        </button>
+      )}
     </div>
   );
 };
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,14 +1,64 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Login = () => {
   const [state, setState] = useState("Sign Up");
-  const [name,setName] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const { backendUrl, setIsLoggedin, getUserData } = useContext(AppContext);
+
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+
+      axios.defaults.withCredentials = true
+
+      if (state === "Sign Up") {
+        const { data } = await axios.post(backendUrl + "/api/auth/register", {
+          name,
+          email,
+          password,
+        });
+
+        if (data.success) {
+          setIsLoggedin(true)
+          getUserData()
+          navigate('/')
+        }else{
+          toast.error(data.message)
+        }
+      } else {
+        const { data } = await axios.post(backendUrl + "/api/auth/login", {
+          email,
+          password,
+        });
+
+        if (data.success) {
+          setIsLoggedin(true)
+          getUserData();
+          navigate('/')
+        }else{
+          toast.error(data.message)
+        }
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  };
 
   return (
     <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400">
       <img
+        onClick={() => navigate("/")}
         src={assets.logo}
         className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
         alt=""
@@ -22,11 +72,13 @@ const Login = () => {
             ? "Create your account"
             : "Login to your account"}
         </p>
-        <form>
+        <form onSubmit={onSubmitHandler}>
           {state === "Sign Up" && (
             <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
               <img src={assets.person_icon} alt="" />
               <input
+                onChange={(e) => setName(e.target.value)}
+                value={name}
                 className="bg-transparent outline-none"
                 type="text"
                 placeholder="Full Name"
@@ -38,6 +90,8 @@ const Login = () => {
           <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
             <img src={assets.mail_icon} alt="" />
             <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
               className="bg-transparent outline-none"
               type="email"
               placeholder="Email id"
@@ -47,13 +101,18 @@ const Login = () => {
           <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
             <img src={assets.lock_icon} alt="" />
             <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
               className="bg-transparent outline-none"
               type="password"
               placeholder="Password"
               required
             />
           </div>
-          <p className="mb-4 text-indigo-400 cursor-pointer">
+          <p
+            onClick={() => navigate("/reset-password")}
+            className="mb-4 text-indigo-400 cursor-pointer"
+          >
             Forgot Password?
           </p>
           <button className="w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900 font-medium">

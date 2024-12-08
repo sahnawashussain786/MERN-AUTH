56a1b926e0b815c3d2bd449ad4be4920eac24c7d
Sahnawas Hussain <142085109+sahnawashussain786@users.noreply.github.com>
2024-12-08 11:21:09 +0530

    completed navbar and header

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Header from '../components/Header'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-[url("/bg_img.png")] bg-cover bg-center'>
+      <Navbar />
+      <Header />
     </div>
   )
 }

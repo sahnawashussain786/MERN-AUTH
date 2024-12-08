8374923a2442af22cc0daa433b7fe51dfa68207a
Sahnawas Hussain <142085109+sahnawashussain786@users.noreply.github.com>
2024-12-08 10:59:55 +0530

    setup the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import EmailVerify from './pages/EmailVerify'
+import ResetPassword from './pages/ResetPassword'
 
 const App = () => {
   return (
     <div>
-      App
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/email-verify' element={<EmailVerify/>}/>
+        <Route path='/reset-password' element={<ResetPassword/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ client/src/pages/ResetPassword.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const ResetPassword = () => {
+  return (
+    <div>
+      <h1>ResetPassword</h1>
+    </div>
+  )
+}
+
+export default ResetPassword

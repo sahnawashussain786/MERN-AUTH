a070bba50a13b4c460c7c824721cce4ee116b17c
Sahnawas Hussain <142085109+sahnawashussain786@users.noreply.github.com>
2024-12-08 18:29:35 +0530

    completed email verification functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,6 +12,23 @@ const Navbar = () => {
     AppContext
   );
 
+  const sendVerificationOtp = async () => {
+    try {
+      axios.defaults.withCredentials = true;
+
+      const {data} = await axios.post(backendUrl + '/api/auth/send-verify-otp')
+
+      if (data.success) {
+        navigate('/email-verify');
+        toast.success(data.message)
+      }else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
   const logout = async () => {
     try {
       axios.defaults.withCredentials = true
@@ -33,7 +50,7 @@ const Navbar = () => {
           {userData.name[0].toUpperCase()}
           <div className="absolute hidden group-hover:block top-0 ring-0 z-10 text-black rounded pt-10">
             <ul className="list-none m-0 p-2 bg-gray-100 text-sm rounded-md">
-              {!userData.isAccountVerified && <li className="py-1 px-2 hover:bg-gray-200 cursor-pointer rounded">Verify Email</li>}
+              {!userData.isAccountVerified && <li onClick={sendVerificationOtp} className="py-1 px-2 hover:bg-gray-200 cursor-pointer rounded">Verify Email</li>}
               <li onClick={logout} className="py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10 rounded">Logout</li>
             </ul>
           </div>

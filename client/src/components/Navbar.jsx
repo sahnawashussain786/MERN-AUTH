6522f13ce69979d617e9de3bfa7a01cd6ba95e1d
Sahnawas Hussain <142085109+sahnawashussain786@users.noreply.github.com>
2024-12-08 14:46:40 +0530

    completed login and logout func

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,12 +3,27 @@ import React, { useContext } from "react";
 import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Navbar = () => {
   const navigate = useNavigate();
   const { userData, backendUrl, setUserData, setIsLoggedin } = useContext(
     AppContext
   );
+
+  const logout = async () => {
+    try {
+      axios.defaults.withCredentials = true
+      const {data} = await axios.post(backendUrl + '/api/auth/logout')
+
+      data.success && setIsLoggedin(false)
+      data.success && setUserData(false)
+      navigate('/')
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
 
   return (
     <div className="w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0">
@@ -17,9 +32,9 @@ const Navbar = () => {
         <div className="w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group">
           {userData.name[0].toUpperCase()}
           <div className="absolute hidden group-hover:block top-0 ring-0 z-10 text-black rounded pt-10">
-            <ul className="">
-              <li>Verify Email</li>
-              <li>Logout</li>
+            <ul className="list-none m-0 p-2 bg-gray-100 text-sm rounded-md">
+              {!userData.isAccountVerified && <li className="py-1 px-2 hover:bg-gray-200 cursor-pointer rounded">Verify Email</li>}
+              <li onClick={logout} className="py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10 rounded">Logout</li>
             </ul>
           </div>
         </div>
